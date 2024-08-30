2a7a72b86c93431932ef13151c43502b490d9a00
Jeong-gyuKim <jgsteve104@gmail.com>
2024-08-31 06:05:55 +0900

    refactor

--- hist.py
+++ hist.py
@@ -1,27 +1,24 @@
-from ising import read
+from ising import read, get_Z, get_avg, Cv, chi, dm_dbeta, binder, dbinder_dbeta
 import numpy as np
 import matplotlib.pyplot as plt
 
+L = 12
+
 data = read('data.pickle')
-
-L = 4
-origin = 2.9
-n=0
-
-ori = data[L][origin][n]
-ori_E = np.array(sorted(list(ori.keys())))
-ori_cnt = np.array([ori[E][0] for E in ori_E])
-
-T = []
-E = []
-for target in np.arange(0.01,8,0.01):
-    Z = np.array([np.exp((1./origin - 1./target) * E) for E in ori_E])
-    hist_cnt = ori_cnt * Z
-    mean = sum(ori_E * hist_cnt) / sum(hist_cnt)
-
-    T.append(target)
-    E.append(mean/L**2)
-    
-plt.scatter(T,E, label='{}'.format(L))
-plt.legend()
+T1, energy, hist_E, hist_M, hist_M2, hist_M4 = data[L]
+m = []
+for i in range(len(T1)):
+    m.append(sum(np.array(hist_E[i]) * np.array(hist_M[i])/L**2) / sum(hist_E[i]))
+plt.plot(T1, m)
 plt.show()
+'''
+Z1 = get_Z(data, L)
+
+t = []
+e = []
+for T in np.arange(0.01,8, 0.01):
+    s_e, s_e2, s_m, s_m2, s_m4, s_me, s_m2e, s_m4e = get_avg(data, L, T, Z1)
+    t.append(T)
+    e.append(s_m)
+plt.plot(t,e)
+plt.show()'''
--- ising.py
+++ ising.py
@@ -96,37 +96,48 @@ def burn_in(arr, sigma=1.):
 #여러 번 얻은 결과 통계
 #@numba.njit(nopython=True, nogil=True)
 def statistics(Temp_range, L_range, N, n, init_up_rate = 0.5, f=wolff):
-     dict_L = {}#
+     """
+     D#온도 범위
+     E#에너지 범위
+     energy[MAX_D][MAX_E]#에너지 준위 ex)MAX_D번째 온도에서 MAX_E번째 에너지 준위의 에너지 값
+     hist_E[MAX_D][MAX_E]#에너지 개수 ex)MAX_D번째 온도에서 MAX_E번째 에너지 준위의 출현 수
+     hist_M[MAX_D][MAX_E]#평균 M
+     hist_M2[MAX_D][MAX_E]#평균 M^2
+     hist_M4[MAX_D][MAX_E]#평균 M^4
+     T1[MAX_D]#MAX_D번째 온도의 온도 값
+     Z1[MAX_D]#온도에 따른 분배 함수
+     """
+     
+     data = {}
      for L in L_range:
           rate = init_up_rate
-          dict_T = {}#
+          D = []
           for T in Temp_range:
-               dict_n = {}#
-               magnetizations_data = np.zeros(n, dtype=np.float64)
-               for j in range(n):
-                    energies, magnetizations = f(L, rate, N, T)
-                    dict_E = hist_dict(energies, magnetizations)#
-                    magnetizations_data[j] = np.mean(np.absolute(magnetizations))/L**2
-                    dict_n[j]=dict_E
-               rate = np.mean(magnetizations_data)
-               rate = (rate + 1) / 2
+               energies, magnetizations = f(L, rate, N, T)
+               energy, hist_E, hist_M, hist_M2, hist_M4 = hist_dict(energies, magnetizations)
+               rate = (np.mean(np.absolute(magnetizations))/L**2 + 1) / 2
                print(L, T)
-               dict_T[T]=dict_n
-          dict_L[L]=dict_T
-     return dict_L
+               D.append([T, energy, hist_E, hist_M, hist_M2, hist_M4])
+          data[L] = [(list(x)) for x in zip(*D)]
+     return data
 
 def hist_dict(energies, magnetizations):
      energies, magnetizations = burn_in(energies), burn_in(magnetizations)
      l = min(len(energies), len(magnetizations))
-     energies, magnetizations = energies[-l:], magnetizations[-l:]
+     energies, magnetizations = energies[-l:], abs(magnetizations[-l:])
      
-     dict_E = {}
+     dict = {}
      for i, e in enumerate(energies):
-          if dict_E.get(e):
-               dict_E[e] = update_dict(dict_E[e], magnetizations[i])
+          if dict.get(e):
+               dict[e] = update_dict(dict[e], magnetizations[i])
           else:
-               dict_E[e] = update_dict([0,0,0,0], magnetizations[i])
-     return dict_E
+               dict[e] = update_dict([0,0,0,0], magnetizations[i])
+     
+     energy = list(dict.keys())
+     hist = list(dict.values())
+     hist_E, hist_M, hist_M2, hist_M4 = [np.array(list(x)) for x in zip(*hist)]
+     
+     return energy, hist_E, hist_M, hist_M2, hist_M4
                
 def update_dict(li, magnetization):
      M, M2, M4 = magnetization, magnetization**2, magnetization**4
@@ -145,3 +156,88 @@ def read(filename):
      with open(filename,'rb') as fr:
           data = pickle.load(fr)
      return data
+
+def get_Z(data, L):
+     
+     T1, energy, hist_E, hist_M, hist_M2, hist_M4 = data[L]
+     size_d = len(T1)
+     size_e = np.array([len(e) for e in energy])
+     
+     Z1 = np.ones(size_d)
+     Z2 = np.zeros(size_d)
+     for iter in range(50):
+          for D in range(size_d):
+               for i in range(size_d):
+                    for k in range(size_e[i]):
+                         sZ = 0.
+                         for j in range(size_d):
+                              sZ += np.exp((1./T1[D] - 1./T1[j]) * energy[i][k])/Z1[j]
+                         Z2[D] += hist_E[i][k] / sZ
+          tZ = sum(Z2)
+          diff = 0.
+          for D in range(size_d):
+               Z2[D] = Z2[D]/tZ
+               diff += abs(Z1[D] - Z1[D])
+               Z1[D] = Z2[D]
+          if diff<0.0001:
+               break
+     return Z1
+
+def get_avg(data, L, T, Z1):
+     
+     T1, energy, hist_E, hist_M, hist_M2, hist_M4 = data[L]
+     size_d = len(T1)
+     size_e = np.array([len(e) for e in energy])
+     Nsite = L**2
+
+     s_e, s_e2, s_m, s_m2, s_m4, s_me, s_m2e, s_m4e, tZ = [0 for _ in range(9)]
+     for D in range(size_d):
+          for i in range(size_e[D]):
+               e1 = energy[D][i]/Nsite
+               m1 = hist_M[D][i]
+               m2 = hist_M2[D][i]
+               m4 = hist_M4[D][i]
+               Z = Z1[D] * hist_E[D][i]*np.exp(-energy[D][i]*(1./T - 1./T1[D]))
+               
+               s_e += e1*Z
+               s_e2 += e1*e1*Z
+               s_m += m1*Z
+               s_m2 += m2*Z
+               s_m4 += m4*Z
+               s_me += m1*e1*Z
+               s_m2e += m2*e1*Z
+               s_m4e += m4*e1*Z
+               tZ += Z
+     s_e, s_e2, s_m, s_m2, s_m4, s_me, s_m2e, s_m4e = s_e/tZ, s_e2/tZ, s_m/tZ, s_m2/tZ, s_m4/tZ, s_me/tZ, s_m2e/tZ, s_m4e/tZ
+
+     return s_e, s_e2, s_m, s_m2, s_m4, s_me, s_m2e, s_m4e
+
+
+def Cv(data, L, T, Z1):
+    Nsite = L**2
+    s_e, s_e2, s_m, s_m2, s_m4, s_me, s_m2e, s_m4e = get_avg(data, L, T, Z1)
+    return Nsite * (s_e2 - s_e * s_e) / (T * T)
+
+
+def chi(data, L, T, Z1):
+    Nsite = L**2
+    s_e, s_e2, s_m, s_m2, s_m4, s_me, s_m2e, s_m4e = get_avg(data, L, T, Z1)
+    return Nsite * (s_m2 - s_m * s_m) / T
+
+
+def dm_dbeta(data, L, T, Z1):
+    Nsite = L**2
+    s_e, s_e2, s_m, s_m2, s_m4, s_me, s_m2e, s_m4e = get_avg(data, L, T, Z1)
+    return Nsite * (s_m * s_e - s_me)
+
+
+def binder(data, L, T, Z1):
+    s_e, s_e2, s_m, s_m2, s_m4, s_me, s_m2e, s_m4e = get_avg(data, L, T, Z1)
+    return 1.0 - s_m4 / (s_m2 * s_m2 * 3.0)
+
+
+def dbinder_dbeta(data, L, T, Z1):
+    Nsite = L**2
+    s_e, s_e2, s_m, s_m2, s_m4, s_me, s_m2e, s_m4e = get_avg(data, L, T, Z1)
+    return (Nsite * (s_m2 * (s_m4e + s_m4 * s_e) - 2.0 * s_m4 * s_m2e) /
+            (3.0 * s_m2 * s_m2 * s_m2))
